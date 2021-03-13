a6c8dda8cb550a97c1c82f472b6678ba22535efa
maxide201 <maxide201@ya.ru>
2021-03-14 02:37:20 +0300

    added Swagger

--- serverAPI/serverAPI/Controllers/UsersController.cs
+++ serverAPI/serverAPI/Controllers/UsersController.cs
@@ -20,6 +20,11 @@ namespace serverAPI.Controllers
             this.db = db;
         }
 
+        /// <summary>
+        /// Return user by id.
+        /// </summary>
+        /// <response code="200">Returns user</response>
+        /// <response code="404">If the user doesn't exist</response>
         [HttpGet("{id}")]
         public JsonResult GetUser(uint id)
         {
@@ -31,7 +36,28 @@ namespace serverAPI.Controllers
             return new JsonResult(Ok(user));
 
         }
-  
+
+        /// <summary>
+        /// Creates a new user.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Users
+        ///     {
+        ///        "purchases": [
+        ///             {
+        ///                 "name": "apple",
+        ///                 "purchaseDate": "2000-01-01T01:01:01",
+        ///                 "cost: 1000
+        ///             }
+        ///         ]
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>A newly created user</returns>
+        /// <response code="200">Returns the newly created user</response>
+        /// <response code="400">If the user is null or invalid</response>      
         [HttpPost]
         public JsonResult PostUser(User user)
         {
@@ -43,6 +69,36 @@ namespace serverAPI.Controllers
             return new JsonResult(Ok(user));
         }
 
+        /// <summary>
+        /// Update purchases list of existing user.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /Users
+        ///     {
+        ///        "id": 1
+        ///        "purchases": [
+        ///             {
+        ///                 "id": 1,
+        ///                 "userId": 1,
+        ///                 "name": "apple",
+        ///                 "purchaseDate": "2000-01-01T01:01:01",
+        ///                 "cost: 1000
+        ///             },
+        ///             {
+        ///                 "name": "banana",
+        ///                 "purchaseDate": "2000-01-01T01:01:02",
+        ///                 "cost: 200
+        ///             }
+        ///         ]
+        ///     }
+        ///
+        /// </remarks>
+        /// <returns>Updated user</returns>
+        /// <response code="200">Returns updated user</response>
+        /// <response code="400">If the user is null or invalid</response>
+        /// <response code="404">If the user doesn't exist</response>
         [HttpPut]
         public JsonResult PutUser(User user)
         {
@@ -55,6 +111,11 @@ namespace serverAPI.Controllers
             return new JsonResult(Ok(user));
         }
 
+        /// <summary>
+        /// Delete user by id.
+        /// </summary>
+        /// <response code="200">Returns deleted user</response>
+        /// <response code="404">If the user doesn't exist</response>
         [HttpDelete("{id}")]
         public JsonResult DeleteUser(uint id)
         {
--- serverAPI/serverAPI/Startup.cs
+++ serverAPI/serverAPI/Startup.cs
@@ -9,6 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 
 namespace serverAPI
 {
@@ -25,6 +28,25 @@ namespace serverAPI
             string connectionString = Configuration.GetConnectionString("Database");
             services.AddTransient<IUserRepository, UserRepository>(provider => new UserRepository(connectionString));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "serverAPI API",
+                    Description = "Backend API for level 1 task\nfrom RTU IT LAB",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Maxim Soluyanov",
+                        Email = string.Empty,
+                        Url = new Uri("https://vk.com/maxide201"),
+                    },
+                });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -34,6 +56,13 @@ namespace serverAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+            });
 
             app.UseHttpsRedirection();
 
